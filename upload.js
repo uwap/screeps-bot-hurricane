4300c092cff2987de60bdb81a65c1a88e48edeb7
uwap <me@uwap.name>
2022-12-20 09:13:21 +0100

    add upload script

---
+++ upload.js
@@ -0,0 +1,25 @@
+
+require('dotenv').config();
+const fs = require('fs');
+var https = require('https');
+
+var data = {
+    branch: 'world',         
+    modules: {
+        main: fs.readFileSync('./dist/screeps.js', 'utf-8').toString(),
+    }
+};
+
+var req = https.request({
+    hostname: 'screeps.com',
+    port: 443,
+    path: '/api/user/code',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-Token': process.env.SCREEPS_API_TOKEN
+    }
+});
+
+req.write(JSON.stringify(data));
+req.end();
